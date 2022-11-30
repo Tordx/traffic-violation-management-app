19f2aa78345c3c7c98cdc37d1f6a223e36139aad
ROVEN KYLE <ellamateogang@gmail.com>
2022-12-01 02:17:31 +0800

    Add: Added StatusBar

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import Rooting from './source/Rooting'
 
 export default function App() {
   return (
     <>
+    <StatusBar
+    barStyle={'dark-content'}
+    backgroundColor = {''}
+    />
     <Rooting/>
     </>
     
