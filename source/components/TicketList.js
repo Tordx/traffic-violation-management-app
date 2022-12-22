3275a4fc01b9a40fd0f2489c85fed29b6e581b52
ROVEN KYLE <ellamateogang@gmail.com>
2022-12-22 21:24:24 +0800

    Added Ticketing list data

--- source/components/TicketList.js
+++ source/components/TicketList.js
@@ -1,61 +1,54 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, TouchableOpacity, ListViewComponent, SectionList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+  
+  SafeAreaView, 
+  View, 
+  FlatList, 
+  StyleSheet, 
+  Text, 
+  TouchableOpacity, 
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    rn: '56665124',
-    fullname: 'John Doe',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    rn: '56655001',
-    fullname: 'Mary Grace',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    rn: '56635331',
-    fullname: 'Jayson Fernandez',
-  },
-  {
-    id: 'bd7acbe2a-c1b1-46c2-aed5-3ad53abb28ba',
-    rn: '56615125',
-    fullname: 'Grazziel Garcia',
-  },
-  {
-    id: '3ac68af3c-c605-48d3-a4f8-fbd91aa97f63',
-    rn: '56651631',
-    fullname: 'Jerrylyn Bugarin',
-  },
-  {
-    id: '586944a0f-3da1-471f-bd96-145571e29d72',
-    rn: '56653312',
-    fullname: 'Charlote Mose',
-  },
-  {
-    id: 'bd17acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    rn: '56652678',
-    fullname: 'Ryan Cezar vallo',
-  },
-  {
-    id: '3ac68a1fc-c605-48d3-a4f8-fbd91aa97f63',
-    rn: '56656357',
-    fullname: 'Bonn Jovi',
-  },
-  {
-    id: '58694a0f-3d5a1-471f-bd96-145571e29d72',
-    rn: '56657864',
-    fullname: 'Hekhek',
-  },
-];
+} from 'react-native';
+import { remoteDBViolation } from '../Database/pouchDB';
+
 export const TicketingList = () => {
+
+  useEffect(() => {
+    rendertickets()
+  })
+
+  const [mytickets, setNewTickets] = useState('');
+
+  const rendertickets = async() => {
+
+    var result = await remoteDBViolation.allDocs({
+      include_docs: true,
+      attachments: true,
+    });
+    if(result.row){
+      let modifiedArr =result.rows.map(function(item){
+        return doc
+      });
+      let filteredData = modifiedArr.filter(item => {
+        return item;
+      });
+      if (filteredData) {
+        let newFilterData = filteredData.map(item => {
+            return item;
+        })
+        setNewTickets(newFilterData)
+      }
+    }
+
+  }
+  
 
   const renderItem = ({ item }) => (
  
     <View style={styles.item}> 
       <TouchableOpacity>
-        <Text style={styles.title}>#{item.rn}</Text>
-        <Text style={styles.name}>{item.fullname}</Text>
+        <Text style={styles.title}>#{item.LicenseNumber}</Text>
+        <Text style={styles.name}>{item.drivername}</Text>
       </TouchableOpacity>
       
     </View>
@@ -66,7 +59,7 @@ export const TicketingList = () => {
     <SafeAreaView style={styles.container}>
      
       <FlatList
-        data={DATA}
+        data={mytickets}
         renderItem={renderItem}
         keyExtractor={item => item.id}
       />
