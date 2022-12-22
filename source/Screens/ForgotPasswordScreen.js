1595e017c5b9bb335fc57b072eae1bcbd89987da
ROVEN KYLE <ellamateogang@gmail.com>
2022-12-23 03:32:46 +0800

    Added forgotpasswordScreen

---
+++ source/Screens/ForgotPasswordScreen.js
@@ -0,0 +1,27 @@
+import { View, Text, BackHandler, StatusBar } from 'react-native'
+import React, {useEffect} from 'react'
+import { useNavigation } from '@react-navigation/native';
+export default function ForgotPasswordScreen() {
+
+   const navigation = useNavigation();
+
+    useEffect(() => {
+        BackHandler.addEventListener("hardwareBackPress", backAction);
+    
+        return () =>
+          BackHandler.removeEventListener("hardwareBackPress", backAction);
+      }, []);
+
+      const backAction = () => {
+            navigation.goBack('LoginScreen')
+        return true;
+      };
+
+  return (
+    <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F4EAE6'}}>
+        <StatusBar
+        backgroundColor={'#F4EAE6'}/>
+      <Text>For forgotten Password, Please contact your Adminstrator </Text>
+    </View>
+  )
+}
