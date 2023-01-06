f759399ea9c52b5b1db4fcaedc5defd73aed3ae3
ROVEN KYLE <ellamateogang@gmail.com>
2023-01-06 21:42:12 +0800

    fixed popup screen

--- source/components/Popup.js
+++ source/components/Popup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Modal, Pressable } from 'react-native';
+import { View, Text, Button, Modal, Pressable, ImageBackground, TouchableOpacity } from 'react-native';
 import Icon  from 'react-native-vector-icons/MaterialIcons';
 import { useSelector } from 'react-redux';
+import { Black } from '../Assets/colors';
 
 export const Popup = () => {
 
@@ -12,28 +13,39 @@ export const Popup = () => {
     <View>
         
       <Modal
-        animationType="fade"
+        animationType="slide"
         transparent={true}
         visible={isModalVisible}
         onRequestClose={() => setModalVisible(false)}
       >
       
             
-        <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}>
+        <View 
+        
+        style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', backgroundColor: '#00000069'}}>
       
-          <View style = {{width: '80%', height: '70%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderRadius: 20,}}>
-                    
-            <Pressable
-                style = {{position: 'absolute', top: 10, right: 10}}
+        <View style = {{width: '80%', height: '50%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderRadius: 20,}}>
+          
+           
+          <ImageBackground
+            resizeMode='contain'
+            style={{height: '100%', width: '100%', position: 'absolute', bottom: 0,  justifyContent: 'center', alignItems: 'center'}}
+            source={require('../Assets/Images/waving-officer.png')}
+            >
+            <Text style = {{fontSize: 17, fontFamily: 'codenext-semibold', color: Black, position: 'absolute', bottom: 20}}>WELCOME TO TVM APP</Text>
+            
+          </ImageBackground>
+          
+          </View>
+          <TouchableOpacity
+                style = {{justifyContent: 'center', alignItems: 'center',position: 'absolute', bottom: 100, borderRadius: 100, width: 35, height: 35, borderColor: '#fff', borderWidth: 1}}
                 onPress={() => setModalVisible(false)}>
                 <Icon
                 name='close'
-                size={35}
+                size={30}
+                color = {'#fff'}
                 />
-            </Pressable>
-            <Text style = {{fontSize: 20}}>Welcome back {username}!</Text>
-            
-          </View>
+            </TouchableOpacity>
         </View>
         
       </Modal>
