d58d6d17d085c865d96873f9c12fad742fd2f850
ROVEN KYLE <ellamateogang@gmail.com>
2023-01-10 02:56:28 +0800

    change data collection to fullname

--- source/components/Popup.js
+++ source/components/Popup.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Modal, Pressable, ImageBackground, TouchableOpacity } from 'react-native';
+import { 
+  
+  View, 
+  Text, 
+  Button, 
+  Modal, 
+  Pressable, 
+  ImageBackground, 
+  TouchableOpacity 
+
+} from 'react-native';
 import Icon  from 'react-native-vector-icons/MaterialIcons';
 import { useSelector } from 'react-redux';
 import { Black } from '../Assets/colors';
 
 export const Popup = () => {
 
-    const {username} = useSelector((store) => store.login)
+    const {fullname} = useSelector((store) => store.login)
     const [isModalVisible, setModalVisible] = useState(true);
 
   return (
@@ -32,8 +42,10 @@ export const Popup = () => {
             style={{height: '100%', width: '100%', position: 'absolute', bottom: 0,  justifyContent: 'center', alignItems: 'center'}}
             source={require('../Assets/Images/waving-officer.png')}
             >
-            <Text style = {{fontSize: 17, fontFamily: 'codenext-semibold', color: Black, position: 'absolute', bottom: 20}}>WELCOME TO TVM APP</Text>
-            
+              <View style = {{position: 'absolute', bottom: 20}}>
+                <Text style = {{fontSize: 17, fontFamily: 'codenext-semibold', color: Black, textAlign: 'center' }}>Hi {fullname}!</Text>
+                <Text style = {{fontSize: 17, fontFamily: 'codenext-semibold', color: Black, textAlign: 'center' }}>WELCOME TO TVM APP</Text>
+              </View>
           </ImageBackground>
           
           </View>
